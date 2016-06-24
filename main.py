3206a98c7f265803e6baaf7f4430d56c6c092622
dign50501 <dign50501@naver.com>
2016-06-25 04:58:58 +0900

    1.032

--- main.py
+++ main.py
@@ -1,5 +1,228 @@
+import sys
+sys.setrecursionlimit(1500)
+
 count_user = 0
 count_word = 0
+
+WHITE = 0
+GRAY = 1
+BLACK = 2
+
+
+class Adj:
+    def __init__(self):
+        self.n = 0
+        self.next = None
+
+
+class Vertex:
+    def __init__(self, name):
+        self.color = WHITE
+        self.parent = -1
+        self.name = name
+        self.n = 0
+        self.first = None
+
+    def add(self, v):
+        a = Adj()
+        a.n = v.n
+        a.next = self.first
+        self.first = a
+
+    def copy(self, other):
+        self.color = other.color
+        self.parent = other.parent
+        self.name = other.name
+        self.n = other.n
+        self.first = other.first
+
+
+class DFSVertex(Vertex):
+    def __init__(self, name):
+        super().__init__(name)
+        self.d = 0
+        self.f = 0
+
+    def copy(self, other):
+        super().copy(other)
+        self.d = other.d
+        self.f = other.f
+
+
+class Queue:
+    def __init__(self):
+        self.front = 0
+        self.rear = 0
+        self.sz = 0
+        self.buf = []
+
+    def create_queue(self,sz):
+        self.sz = sz
+        self.buf = list(range(sz))  # malloc(sizeof(int)*sz)
+
+    def enqueue(self,val):
+        self.buf[self.rear] = val
+        self.rear = (self.rear + 1) % self.sz
+
+    def dequeue(self):
+        res = self.buf[self.front]
+        self.front = (self.front + 1) % self.sz
+        return res
+
+    def is_empty(self):
+        return self.front == self.rear
+
+
+def print_vertex(vertices,n):
+    print(vertices[n].name, end=' ')
+    print(vertices[n].color, end=' ')
+    print(vertices[n].parent, end=' ')
+    print(vertices[n].d, end=':')
+    p = vertices[n].first
+    while p:
+        print(vertices[p.n].name, end = ' ')
+        p = p.next
+    print('')
+
+
+def g_transpose(vertices, vertices1):
+    for i in range(len(vertices1)):
+        vertices1[i].first = None
+    for v in vertices:
+        p = v.first
+        while p:
+            vertices1[p.n].add(v)
+            p = p.next
+
+
+class DepthFirstSearch:
+    def __init__(self):
+        self.time = 0
+        self.vertices = None
+
+    def set_vertices(self, vertices):
+        self.vertices = vertices
+        for i in range(len(self.vertices)):
+            self.vertices[i].n = i
+
+    def dfs(self):
+        for u in self.vertices:
+            u.color = WHITE
+            u.parent = -1
+        self.time = 0
+        for u in self.vertices:
+            if u.color == WHITE:
+                self.dfs_visit(u)
+
+    def dfs_visit(self, u):
+        self.time += 1
+        u.d = self.time
+        u.color = GRAY
+        v = u.first
+        while v:
+            if self.vertices[v.n].color == WHITE:
+                self.vertices[v.n].parent = u.n
+                self.dfs_visit(self.vertices[v.n])
+            v = v.next
+        u.color = BLACK
+        self.time += 1
+        u.f = self.time
+
+    def print_scc(self, u):
+        print(u.name, end=" ")
+        vset = self.vertices
+        if u.parent >= 0:
+            self.print_scc(vset[u.parent])
+
+    def scc_find(self, u):
+        u.color = GRAY
+        v = u.first
+        found = False
+        while v:
+            if self.vertices[v.n].color == WHITE:
+                found = True
+                self.vertices[v.n].parent = u.n
+                self.scc_find(self.vertices[v.n])
+            v = v.next
+        if not found:
+            print("SCC:", end=" ")
+            self.print_scc(u)
+            print("")
+        u.color = BLACK
+
+    def print_vertex(self,n):
+        print(self.vertices[n].name, end=' ')
+        print(self.vertices[n].color, end=' ')
+        print(self.vertices[n].parent, end=' ')
+        print(self.vertices[n].d, end=' ')
+        print(self.vertices[n].f, end=':')
+        p = self.vertices[n].first
+        while p:
+            print(self.vertices[p.n].name, end = ' ')
+            p = p.next
+        print('')
+
+    def print_vertices(self):
+        for i in range(len(self.vertices)):
+            self.print_vertex(i)
+
+    def transpose(self):
+        vertices1 = []
+        for v in self.vertices:
+            v1 = DFSVertex(v.name)
+            v1.copy(v)
+            vertices1.append(v1)
+        g_transpose(self.vertices, vertices1)
+        self.set_vertices(vertices1)
+
+    def left(self, n):
+        return 2*n+1
+
+    def right(self, n):
+        return 2*n+2
+
+    def heapify(self, A, i, heapsize):
+        vset = self.vertices
+        l = self.left(i)
+        r = self.right(i)
+        if l < heapsize and vset[A[l]].f < vset[A[i]].f:
+            largest = l
+        else:
+            largest = i
+        if r < heapsize and vset[A[r]].f < vset[A[largest]].f:
+            largest = r
+        if largest != i:
+            A[i], A[largest] = A[largest], A[i]
+            self.heapify(A, largest, heapsize)
+
+    def buildheap(self, A):
+        for i in range(len(A)//2 + 1, 0, -1):
+            self.heapify(A, i-1, len(A))
+
+    def heapsort(self, A):
+        self.buildheap(A)
+        for i in range(len(A), 1, -1):
+            A[i-1],A[0] = A[0], A[i-1]
+            self.heapify(A, 0, i - 1)
+
+    def sort_by_f(self):
+        vset = self.vertices
+        sorted_indices = list(range(len(vset)))
+        self.heapsort(sorted_indices)
+        return sorted_indices
+
+    def scc(self):
+        self.dfs()
+        #self.print_vertices()
+        self.transpose()
+        sorted = self.sort_by_f()
+        vset = self.vertices
+        for v in vset:
+            v.color = WHITE
+            v.parent = -1
+        for n in sorted:
+            if self.vertices[n].color == WHITE:
+                self.scc_find(vset[n])
 
 
 class RBNode:
@@ -340,6 +563,14 @@ class UserRBtree(RBtree):
 
     total_friends = property(fget=lambda self: self._total_friends)
 
+    def find_all_user_name(self, vertex, x=None):
+        if x is None:
+            x = self.Root
+        if x != self.nil:
+            self.find_all_user_name(vertex, x.left)
+            vertex.append(x.key)
+            self.find_all_user_name(vertex,x.right)
+
     def insert_node(self, z):
         RBtree.insert_node(self, z)
         self._num_of_users += 1
@@ -540,7 +771,7 @@ class ReadData:
 
     @staticmethod
     def word(word_t, user_t):
-        with open('/Users/yeongjin/Downloads/utf-8 2/word.txt') as f:
+        with open('word.txt') as f:
             while True:
                 person = f.readline()
                 person = person[0:-1]
@@ -683,7 +914,31 @@ Select Menu: """, end='')
             else:
                 print("Nothing to delete!")
         elif number == '8':
-            print("8")
+            vertex = []
+            user_tree.find_all_user_name(vertex)
+
+            dfs_ver = [None] * user_tree.num_of_users
+            count = 0
+            dfs_dic = {}
+
+            for i in vertex:
+                dfs_ver[count] = DFSVertex(i)
+                count += 1
+
+            for i in dfs_ver:
+                dfs_dic[i.name] = i
+
+            DFS = DepthFirstSearch()
+            DFS.set_vertices(dfs_ver)
+            for i in range(0, user_tree.num_of_users):
+                x = user_tree.search(dfs_ver[i].name)  # friend of i (i is user_id)
+                for friend in x.friends:
+                    a = dfs_dic.get(friend)
+                    dfs_ver[i].add(a)
+
+            print("-------- Strongly Connected Components -------")
+            DFS.scc()
+
         elif number == '9':
             print("Total users: %d" % user_tree.num_of_users)
         elif number == '99':
