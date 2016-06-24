9c338b751bec75e01cf381a8157eced98f66fb25
dign50501 <dign50501@naver.com>
2016-06-25 06:30:44 +0900

    1.04
    did all

--- main.py
+++ main.py
@@ -9,15 +9,146 @@ GRAY = 1
 BLACK = 2
 
 
-class Adj:
+INFTY = 1E10
+
+
+class Heap:
     def __init__(self):
-        self.n = 0
+        self.nelem = 0
+        self.A = []
+
+    def parent(self, n):
+        return (n-1) // 2
+
+    def left(self, n):
+        return 2 * n + 1
+
+    def right(self, n):
+        return 2 * n + 2
+
+    def compare(self, a, b):
+        return a - b > 0
+
+    def exchange(self, i, j):
+        A = self.A
+        A[i], A[j] = A[j], A[i]
+
+    def heapify(self, i):
+        A = self.A
+        l = self.left(i)
+        r = self.right(i)
+        if l < self.nelem and self.compare(A[l], A[i]):
+            largest = l
+        else:
+            largest = i
+        if r < self.nelem and self.compare(A[r], A[largest]):
+            largest = r
+        if largest != i:
+            self.exchange(i, largest)
+            self.heapify(largest)
+
+
+class PriorNode:
+    def __init__(self, key, n):
+        self.ndx = 0
+        self.n = n
+        self.key = key
+
+    def __repr__(self):
+        return "(%d:%d,%d)" % (self.ndx,self.n, self.key)
+
+
+class MaxQueue(Heap):
+    def __init__(self):
+        super().__init__()
+
+    def compare(self, a, b):
+        return a.key > b.key
+
+    def exchange(self, i, j):
+        A = self.A
+        A[i].ndx = j
+        A[j].ndx = i
+        super().exchange(i, j)
+
+    def update_key(self, i):
+        parent = lambda x: self.parent(i)
+        compare = lambda a,b: self.compare(a, b)
+        A = self.A
+        while i > 0 and not compare(A[parent(i)], A[i]):
+            self.exchange(i, parent(i))
+            i = parent(i)
+
+    def increase_key(self,i,key):
+        A = self.A
+        if key < A[i].key:
+            print ("Error")
+            sys.exit(-1)
+        A[i].key = key
+        self.update_key(i)
+
+    def insert(self,n):
+        A = self.A
+        while len(A) < self.nelem:
+            A.append(None)
+        i = self.nelem
+        A.append(None)
+        self.nelem += 1
+        A[i] = n
+        A[i].ndx = i
+        self.update_key(i)
+
+    def extract(self):
+        elem = self.A[0]
+        self.exchange(0,self.nelem-1)
+        self.nelem -= 1
+        self.heapify(0)
+        return elem
+
+    def is_empty(self):
+        return self.nelem == 0
+
+
+class MinQueue(MaxQueue):
+    def __init__(self):
+        super().__init__()
+
+    def compare(self,a,b):
+        return a.key < b.key
+
+    def update_key(self,i):
+        parent = lambda x: self.parent(i)
+        A = self.A
+        while i > 0 and not self.compare(A[parent(i)], A[i]):
+            self.exchange(i,parent(i))
+            i = parent(i)
+
+    def decrease_key(self,i,key):
+        A = self.A
+        if key > A[i].key:
+            print ("Error")
+            sys.exit(-1)
+        A[i].key = key
+        self.update_key(i)
+
+    def __repr__(self):
+        return "%a %a" % (self.nelem, self.A)
+
+
+class Adj:
+    def __init__(self, n=0): # if does not work delete
+        self.n = n
         self.next = None
+
+
+class Weight(Adj):
+    def __init__(self, n, w):
+        super().__init__(n)
+        self.w = w
 
 
 class Vertex:
     def __init__(self, name):
-        self.color = WHITE
         self.parent = -1
         self.name = name
         self.n = 0
@@ -30,6 +161,111 @@ class Vertex:
         self.first = a
 
     def copy(self, other):
+        self.parent = other.parent
+        self.name = other.name
+        self.n = other.n
+        self.first = other.first
+
+
+class DijkVertex(Vertex):
+    def __init__(self, name):
+        super().__init__(name)
+        self.d = INFTY
+        self.priority = None
+
+    def __repr__(self):
+        return "(%a %a %a)" % (self.name,self.n,self.d)
+
+    def add(self, v, w):
+        a = Weight(v, w)
+        a.next = self.first
+        self.first = a
+
+    def set_priority(self,n):
+        self.priority = n
+
+    def decrease_key(self, q):
+        prio = self.priority
+        ndx = prio.ndx
+        q.decrease_key(ndx, self.d)
+
+
+class Dijkstra:
+    def __init__(self):
+        self.vertices = []
+        self.q = MinQueue()
+
+    def add_vertex(self, name):
+        n = len(self.vertices)
+        v = DijkVertex(name)
+        v.n = n
+        self.vertices.append(v)
+        return v
+
+    def get_vertex(self,name):
+        for v in self.vertices:
+            if v.name == name:
+                return v
+        return None
+
+    def print_vertex(self, n, tree):
+        a = tree.search(self.vertices[n].name)
+        print(a.user_name, end=': ')
+        if self.vertices[n].d == INFTY:
+            print("not reachable")
+        else:
+            print(self.vertices[n].d)
+
+    def print_vertices(self,tree):
+        for i in range(len(self.vertices)):
+            self.print_vertex(i, tree)
+
+    def relax(self, u):
+        vset = self.vertices
+        q = self.q
+        p = u.first
+        while p:
+            v = p.n
+            d = u.d + p.w
+            if d < v.d:
+                v.d = d
+                v.parent = u.n
+                v.decrease_key(q)
+            p = p.next
+
+    def shortest_path(self):
+        q = self.q
+        vset = self.vertices
+        for v in vset:
+            n = PriorNode(v.d, v.n)
+            v.set_priority(n)
+            q.insert(n)
+        while not q.is_empty():
+            u = q.extract()
+            self.relax(vset[u.n])
+
+
+class Adj2:
+    def __init__(self):
+        self.n = 0
+        self.next = None
+
+
+class Vertex2:
+    def __init__(self, name):
+        self.color = WHITE
+        self.parent = -1
+        self.name = name
+        self.n = 0
+        self.first = None
+
+    def add(self, v):
+        a = Adj2()
+        a.n = v.n
+        a.next = self.first
+        self.first = a
+
+    def copy(self, other):
         self.color = other.color
         self.parent = other.parent
         self.name = other.name
@@ -37,7 +273,7 @@ class Vertex:
         self.first = other.first
 
 
-class DFSVertex(Vertex):
+class DFSVertex(Vertex2):
     def __init__(self, name):
         super().__init__(name)
         self.d = 0
@@ -917,18 +1153,14 @@ Select Menu: """, end='')
         elif number == '8':
             vertex = []
             user_tree.find_all_user_name(vertex)
-
             dfs_ver = [None] * user_tree.num_of_users
             count = 0
             dfs_dic = {}
-
             for i in vertex:
                 dfs_ver[count] = DFSVertex(i)
                 count += 1
-
             for i in dfs_ver:
                 dfs_dic[i.name] = i
-
             DFS = DepthFirstSearch()
             DFS.set_vertices(dfs_ver)
             for i in range(0, user_tree.num_of_users):
@@ -936,12 +1168,36 @@ Select Menu: """, end='')
                 for friend in x.friends:
                     a = dfs_dic.get(friend)
                     dfs_ver[i].add(a)
-
             print("-------- Strongly Connected Components -------")
             DFS.scc(user_tree)
-
         elif number == '9':
-            print("Total users: %d" % user_tree.num_of_users)
+            print("Please enter the start user (ex. 105063898)", end='')
+            start_vertex = int(input())
+            g = Dijkstra()
+            vertex = []
+            ver_dic = {}
+            user_tree.find_all_user_name(vertex)
+            vertex_array = [None] * user_tree.num_of_users
+
+            count = 0
+            for i in vertex:
+                vertex_array[count] = g.add_vertex(i)
+                count += 1
+
+            for i in vertex_array:
+                ver_dic[i.name] = i
+
+            for i in range(0, user_tree.num_of_users):
+                x = user_tree.search(vertex_array[i].name)
+                for friend in x.friends:
+                    a = user_tree.search(friend)
+                    b = ver_dic.get(friend)
+                    vertex_array[i].add(b, a.num_of_friends)
+            start_node = ver_dic.get(start_vertex)
+            start_node.d = 0
+            g.shortest_path()
+            g.print_vertices(user_tree)
+
         elif number == '99':
             break
         else:
